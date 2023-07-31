51cb58d8a9ca57cec82d3b982004ff3e22fd0d19
eebrowning <eebrowning828@gmail.com>
2023-07-31 13:24:08 -0400

    google login works, need to make user info available to be stored with comments

--- Backend/app.js
+++ Backend/app.js
@@ -1,13 +1,15 @@
+//Note, Auth stuff crammed here, could clean up on it's own route, but there isn't much to it.
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const session = require('express-session');
+require('dotenv').config();
+const { SESSION_SECRET, CLIENT_SECRET, CLIENT_ID } = process.env;
 const app = express();
 const port = 3000; // You can use any desired port number
 const bodyParser = require('body-parser');
 const cors = require('cors')
 
-const db = require('./db');
-
-//will need a comment route
-//will need a signin route (hopefully via google api)
 const commentRouter = require("./routes/api/comments-router");
 
 app.use((req, res, next) => {
@@ -21,12 +23,66 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors())
 app.use(bodyParser.json())
 
+//base test route
+app.get('/', (req, res) => {//test connection
+    res.send('Hello, backend successfully connected!');
+});
+//Auth Route
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//auth stuff - Google sign-in
+app.use(session({
+    secret: SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+}))
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new GoogleStrategy({
+    clientID: CLIENT_ID,
+    clientSecret: CLIENT_SECRET,
+    callbackURL: '/auth/google/callback',
+    scope: ['profile', 'email'],
+},
+    (accessToken, refreshToken, profile, done) => {
+        console.log('done', profile)
+        console.log('TOKEN', accessToken)
+        return done(null, profile);
+    }
+));
+app.get('/auth/google',
+    passport.authenticate('google', { scope: ['profile', 'email'] })
+);
+
+app.get('/auth/google/callback',
+    passport.authenticate('google', { failureRedirect: '/login' }),
+    (req, res) => {
+        // Successful authentication, redirect to the desired route or perform other actions.
+        res.redirect('/');
+    }
+);
+// app.get('/user',
+//     passport.authenticate('google', { scope: ['profile', 'email'] }),
+//     (req, res) => {
+//         console.log('profile')
+//     }
+
+// );
+
+passport.serializeUser((user, done) => {
+    // Serialize the user data and store it in the session.
+    done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+    // Retrieve the user data from the session.
+    done(null, user);
+});
+////end auth stuff
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
-// app.get('/', (req, res) => {//test connection
-//     res.send('Hello, backend successfully connected!');
-// });
 
+//Comments Route
 app.use('/api/comments', commentRouter)
 
 
