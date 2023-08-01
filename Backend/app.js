c4fc417fdb01dc7ed38a7408110512ed6d43c527
eebrowning <eebrowning828@gmail.com>
2023-08-01 12:06:23 -0400

    working with google sign-in

--- Backend/app.js
+++ Backend/app.js
@@ -5,12 +5,14 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const session = require('express-session');
 require('dotenv').config();
 const { SESSION_SECRET, CLIENT_SECRET, CLIENT_ID } = process.env;
+/////
 const app = express();
-const port = 3000; // You can use any desired port number
+const port = 3000; //
 const bodyParser = require('body-parser');
 const cors = require('cors')
 
 const commentRouter = require("./routes/api/comments-router");
+const usersRouter = require("./routes/api/users-router")
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -30,56 +32,56 @@ app.get('/', (req, res) => {//test connection
 //Auth Route
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //auth stuff - Google sign-in
-app.use(session({
-    secret: SESSION_SECRET,
-    resave: false,
-    saveUninitialized: true,
-}))
-app.use(passport.initialize());
-app.use(passport.session());
-passport.use(new GoogleStrategy({
-    clientID: CLIENT_ID,
-    clientSecret: CLIENT_SECRET,
-    callbackURL: '/auth/google/callback',
-    scope: ['profile', 'email'],
-},
-    (accessToken, refreshToken, profile, done) => {
-        console.log('done', profile)
-        console.log('TOKEN', accessToken)
-        return done(null, profile);
-    }
-));
-app.get('/auth/google',
-    passport.authenticate('google', { scope: ['profile', 'email'] })
-);
-
-app.get('/auth/google/callback',
-    passport.authenticate('google', { failureRedirect: '/login' }),
-    (req, res) => {
-        // Successful authentication, redirect to the desired route or perform other actions.
-        res.redirect('/');
-    }
-);
-// app.get('/user',
-//     passport.authenticate('google', { scope: ['profile', 'email'] }),
-//     (req, res) => {
-//         console.log('profile')
+// app.use(session({
+//     secret: SESSION_SECRET,
+//     resave: false,
+//     saveUninitialized: true,
+// }))
+// app.use(passport.initialize());
+// app.use(passport.session());
+// passport.use(new GoogleStrategy({
+//     clientID: CLIENT_ID,
+//     clientSecret: CLIENT_SECRET,
+//     callbackURL: '/auth/google/callback',
+//     scope: ['profile', 'email'],
+// },
+//     (accessToken, refreshToken, profile, done) => {
+//         console.log('done', profile)
+//         console.log('TOKEN', accessToken)
+//         return done(null, profile);
 //     }
-
+// ));
+// app.get('/auth/google',
+//     passport.authenticate('google', { scope: ['profile', 'email'] })
 // );
 
-passport.serializeUser((user, done) => {
-    // Serialize the user data and store it in the session.
-    done(null, user);
-});
+// app.get('/auth/google/callback',
+//     passport.authenticate('google', { failureRedirect: '/login' }),
+//     (req, res) => {
+//         // Successful authentication, redirect to the desired route or perform other actions.
+//         res.redirect('/');
+//     }
+// );
+// // app.get('/user',
+// //     passport.authenticate('google', { scope: ['profile', 'email'] }),
+// //     (req, res) => {
+// //         console.log('profile')
+// //     }
 
-passport.deserializeUser((user, done) => {
-    // Retrieve the user data from the session.
-    done(null, user);
-});
-////end auth stuff
+// // );
+
+// passport.serializeUser((user, done) => {
+//     // Serialize the user data and store it in the session.
+//     done(null, user);
+// });
+
+// passport.deserializeUser((user, done) => {
+//     // Retrieve the user data from the session.
+//     done(null, user);
+// });
+// ////end auth stuff
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
+app.use('/auth/', usersRouter)
 
 
 //Comments Route
---
+++ Backend/routes/api/users-router.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const session = require('express-session');
+require('dotenv').config();
+const { SESSION_SECRET, CLIENT_SECRET, CLIENT_ID } = process.env;
+
+
+const router = express.Router()
+//Auth Route
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//auth stuff - Google sign-in
+router.use(session({
+    secret: SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+}))
+router.use(passport.initialize());
+router.use(passport.session());
+passport.use(new GoogleStrategy({
+    clientID: CLIENT_ID,
+    clientSecret: CLIENT_SECRET,
+    callbackURL: '/auth/google/callback',
+    scope: ['profile', 'email'],
+},
+    (accessToken, refreshToken, profile, done) => {
+        console.log('done', profile)
+        console.log('TOKEN', accessToken)
+        return done(null, profile);
+    }
+));
+router.get('/google',
+    passport.authenticate('google', { scope: ['profile', 'email'] })
+);
+
+router.get('/google/callback',
+    passport.authenticate('google', { failureRedirect: '/login' }),
+    (req, res) => {
+        // Successful authentication, redirect to the desired route or perform other actions.
+        res.redirect('/');
+    }
+);
+// app.get('/user',
+//     passport.authenticate('google', { scope: ['profile', 'email'] }),
+//     (req, res) => {
+//         console.log('profile')
+//     }
+
+// );
+
+passport.serializeUser((user, done) => {
+    // Serialize the user data and store it in the session.
+    done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+    // Retrieve the user data from the session.
+    done(null, user);
+});
+////end auth stuff
+////////////////////////////////////////////////////////////////////////////////
+module.exports = router;
